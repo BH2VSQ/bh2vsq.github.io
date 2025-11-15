168026655d70841f24f9dec915ba866ba1ad1570
BH2VSQ <BH2VSQ@users.noreply.github.com>
2025-11-15 11:24:43 +0000

    deploy: d33f96f641c11d2f66f37d8719fc043d9d444e34

---
+++ assets/index.DpH7-XZa.js
@@ -0,0 +1,1 @@
+import{A as s}from"./app.DGzsRdDE.js";import"./chunks/dayjs.CPPL_t8N.js";import"./animation.ClOhy8P3.js";const a="/assets/none.DLM1AMsD.jpg";function c(t,o=a){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function i(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{i as f,c as o};
