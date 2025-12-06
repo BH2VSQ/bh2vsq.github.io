00ee26540da7780fb6b1c32763f324fd47b597ac
BH2VSQ <BH2VSQ@users.noreply.github.com>
2025-12-06 11:02:02 +0000

    deploy: 405b3d86a1f7c038025b9f665214f0a23d87dea2

---
+++ assets/back.BJkolGaI.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.GeV7V1ey.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
